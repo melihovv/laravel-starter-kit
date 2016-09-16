17f0330bc538591a92d018dad51e3f41d079bd0d
Alexander Melihov <amelihovv@ya.ru>
2016-09-16 22:18:30 +0400

    move asvae/laravel-api-tester to dev deps

--- config/app.php
+++ config/app.php
@@ -176,7 +176,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        App\Providers\ExceptionServiceProvider::class,
     ],
 
     'local_providers' => [
