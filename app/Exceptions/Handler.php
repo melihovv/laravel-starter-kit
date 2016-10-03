f4d42ad30c4738f177d89b0f0f338511915fd661
Alexander Melihov <amelihovv@ya.ru>
2016-10-03 05:45:24 +0400

    enable phpmd and phpcs check in precommit hook

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -56,6 +56,8 @@ class Handler extends ExceptionHandler
      * @param \Illuminate\Auth\AuthenticationException $exception
      *
      * @return \Illuminate\Http\Response
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     protected function unauthenticated($request, AuthenticationException $exception)
     {
