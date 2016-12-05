a90580aea7e27dd2bc49de19a8d96aa1740b1169
Alexander Melihov <amelihovv@ya.ru>
2016-12-05 22:13:29 +0300

    add setProperty method in TestHelpers

--- tests/TestHelpers.php
+++ tests/TestHelpers.php
@@ -35,9 +35,9 @@ trait TestHelpers
     /**
      * Call protected/private method of a class.
      *
-     * @param object &$object    Instantiated object that we will run method on.
-     * @param string $methodName Method name to call
-     * @param array  $parameters Array of parameters to pass into method.
+     * @param object &$object Instantiated object that we will run method on.
+     * @param string $methodName Method name to call.
+     * @param array $parameters Array of parameters to pass into method.
      *
      * @return mixed Method return.
      */
@@ -51,5 +51,24 @@ trait TestHelpers
         $method->setAccessible(true);
 
         return $method->invokeArgs($object, $parameters);
+    }
+
+    /**
+     * Set protected/private property of a class.
+     *
+     * @param object &$object Instantiated object we set protected property for.
+     * @param string $propertyName Property name to set.
+     * @param mixed $value Value to set.
+     *
+     * @return $this
+     */
+    public function setProperty(&$object, $propertyName, $value)
+    {
+        $reflection = new \ReflectionClass(get_class($object));
+        $reflectionProperty = $reflection->getProperty($propertyName);
+        $reflectionProperty->setAccessible(true);
+        $reflectionProperty->setValue($object, $value);
+
+        return $this;
     }
 }
