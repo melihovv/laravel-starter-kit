95ef85788d5cbb78d02e9c3d63304c7fd62250a0
Alexander Melihov <amelihovv@ya.ru>
2017-02-07 12:34:25 +0300

    add CreatesApplication trait to TestCase
    Signed-off-by: Alexander Melihov <amelihovv@ya.ru>

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,6 +5,7 @@ namespace Tests;
 abstract class TestCase extends \Illuminate\Foundation\Testing\TestCase
 {
     use TestHelpers;
+    use CreatesApplication;
 
     /**
      * The base URL to use while testing the application.
