c6059925596d844e04b006c7a9f4bf83e7d7b32b
Alexander Melihov <amelihovv@ya.ru>
2017-07-16 19:28:51 +0300

    remove some methods from TestHelpers
    Signed-off-by: Alexander Melihov <amelihovv@ya.ru>

--- tests/TestHelpers.php
+++ tests/TestHelpers.php
@@ -19,44 +19,4 @@ trait TestHelpers
 
         return $mock;
     }
-
-    /**
-     * Call protected/private method of a class.
-     *
-     * @param object &$object    Instantiated object that we will run method on.
-     * @param string $methodName Method name to call.
-     * @param array  $parameters Array of parameters to pass into method.
-     *
-     * @return mixed Method return.
-     */
-    protected function invokeMethod(
-        &$object,
-        $methodName,
-        array $parameters = []
-    ) {
-        $reflection = new \ReflectionClass(get_class($object));
-        $method = $reflection->getMethod($methodName);
-        $method->setAccessible(true);
-
-        return $method->invokeArgs($object, $parameters);
-    }
-
-    /**
-     * Set protected/private property of a class.
-     *
-     * @param object &$object      Instantiated object we set protected property for.
-     * @param string $propertyName Property name to set.
-     * @param mixed  $value        Value to set.
-     *
-     * @return $this
-     */
-    public function setProperty(&$object, $propertyName, $value)
-    {
-        $reflection = new \ReflectionClass(get_class($object));
-        $reflectionProperty = $reflection->getProperty($propertyName);
-        $reflectionProperty->setAccessible(true);
-        $reflectionProperty->setValue($object, $value);
-
-        return $this;
-    }
 }
