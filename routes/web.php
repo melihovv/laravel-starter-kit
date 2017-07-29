4b6a5b5c5ba38042ff8e6b38bd815f9a651c6754
Alexander Melihov <amelihovv@ya.ru>
2017-07-29 16:19:56 +0300

    sync with laravel
    Signed-off-by: Alexander Melihov <amelihovv@ya.ru>

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/logs', [
     'uses' => '\Melihovv\LaravelLogViewer\LaravelLogViewerController@index',
     'as' => 'logs',
