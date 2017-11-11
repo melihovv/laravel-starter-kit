91d93d0531adfdc6abf7d0b6cd1f919dcd52569b
Alexander Melihov <amelihovv@ya.ru>
2017-11-11 20:43:32 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,3 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
