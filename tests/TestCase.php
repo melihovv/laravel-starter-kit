1068a63fb34083eeac046e2b346c2c449b49ef77
Alexander Melihov <amelihovv@ya.ru>
2019-06-10 03:37:42 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,8 +2,8 @@
 
 namespace Tests;
 
-use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Mockery;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
