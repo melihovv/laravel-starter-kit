a9ee1a8a45963599753c670339a1c2ec96abe03c
kenneth <kenneth.tomagan123@gmail.com>
2019-10-17 16:01:08 +0800

    Laravel 6 Authentication

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
