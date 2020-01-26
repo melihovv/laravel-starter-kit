cf4094ed976f7f23e874cbbd3d147a390365f54d
Alexander Melihov <amelihovv@ya.ru>
2020-01-26 21:20:54 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,15 +2,15 @@
 
 namespace Tests;
 
-use Mockery;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Mockery;
 
 abstract class TestCase extends BaseTestCase
 {
     use TestHelpers;
     use CreatesApplication;
 
-    protected function setUp() : void
+    protected function setUp(): void
     {
         parent::setUp();
 
